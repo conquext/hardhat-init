c2102417b01a977f2ede0d77bde7ad3a2e010da9
Rasheed <a.rasheedalabi@gmail.com>
2021-08-05 07:03:31 +0100

    Initial setup

---
+++ test/sample-test.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
+
+
+// describe("Token", function () {
+//   let accounts: Signer[];
+
+//   beforeEach(async function () {
+//     accounts = await ethers.getSigners();
+//   });
+
+//   it("should do something right", async function () {
+//     // Do something with the accounts
+//   });
+// });
